a8592f72e0f15568ecaf374b5223c84a46058a2b
Raaed Shaikh <raaed@tuta.io>
2024-06-02 01:15:52 +0530

    challenge 0 done

--- 0_smoke/main.go
+++ 0_smoke/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("hello, world!")
 	tcpListener, err := net.Listen("tcp", ":4242")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create tcp connection: %s", err)
