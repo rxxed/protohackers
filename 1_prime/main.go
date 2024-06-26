c8cabf3ade1c27060338edc4930454a13f26a69a
Raaed Shaikh <raaed@tuta.io>
2024-06-26 15:08:05 +0530

    challenge 1 done

--- 1_prime/main.go
+++ 1_prime/main.go
@@ -5,12 +5,33 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
-	"os"
 )
 
+const PrimeMethodName = "isPrime"
+
+// using this type instead of json.Number because json.Number will allow
+// numbers with quotes like "66" which should actually be considered a malformed request
+type ProtohackersInt int64
+
+func (i *ProtohackersInt) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || b[0] == '"' {
+		return fmt.Errorf("rejecting string")
+	}
+	var f float64
+	err := json.Unmarshal(b, &f)
+	if err != nil {
+		return fmt.Errorf("invalid number")
+	}
+	*i = ProtohackersInt(f)
+	return nil
+}
+
 type Request struct {
-	Method string      `json:"method"`
-	Number json.Number `json:"number"`
+	Method string `json:"method"`
+
+	// this is a pointer and not a value so we can have a way to determine if there was
+	// no number in the request object as this pointer will be `nil` by default.
+	Number *ProtohackersInt `json:"number"`
 }
 
 type Response struct {
@@ -18,10 +39,8 @@ type Response struct {
 	Prime  bool   `json:"prime"`
 }
 
-const PrimeMethodName = "isPrime"
-
 func (r *Request) IsMalformed() bool {
-	if r.Method == "" || r.Number == "" {
+	if r.Method == "" || r.Number == nil {
 		return true
 	}
 	if r.Method != PrimeMethodName {
@@ -42,52 +61,39 @@ func main() {
 			panic("failed to accept connection")
 		}
 
-		defer conn.Close()
+		go handleClient(conn)
+	}
+}
 
-		scanner := bufio.NewScanner(conn)
-		scanner.Split(bufio.ScanLines)
-		for scanner.Scan() {
-			req := Request{}
-			err := json.Unmarshal([]byte(scanner.Text()), &req)
-			if err != nil {
-				// malformed request (invalid json)
-				// nil is sent as malformed response
-				fmt.Fprintln(conn, nil)
-				fmt.Println("malformed request (invalid json)")
-				// break will disconnect client
-				break
-			}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		req := Request{}
+		err := json.Unmarshal([]byte(scanner.Text()), &req)
 
-			fmt.Println("req.Method is empty str?: ", req.Method)
-			fmt.Println("req.Number is empty str?: ", req.Number)
-
-			if req.IsMalformed() {
-				// malformed request (missing required fields or wrong method name)
-				// nil is sent as malformed response
-				fmt.Fprintln(conn, nil)
-				fmt.Println("malformed request (missing required fields or wrong method name)")
-				// break will disconnect client
-				break
-			}
-
-			num, err := req.Number.Int64()
-			if err != nil {
-				// malformed request (req.Number is not a valid numeric value)
-				// nil is sent as malformed response
-				fmt.Fprintln(conn, nil)
-				fmt.Println("malformed request (req.Number is not a valid numeric value)")
-				// break will disconnect client
-				break
-			}
-
-			resp := Response{Method: PrimeMethodName}
-			resp.Prime = isPrime(num)
-			// marshaling of resp will never fail, so ignore the error
-			respBytes, _ := json.Marshal(resp)
-
-			fmt.Fprintln(conn, string(respBytes))
-			fmt.Fprintln(os.Stdout, string(respBytes))
+		if err != nil {
+			// malformed request (invalid json)
+			// nil is sent as malformed response
+			fmt.Fprintln(conn, nil)
+			// break will disconnect client
+			break
 		}
+
+		if req.IsMalformed() {
+			// malformed request (missing required fields / wrong method name)
+			fmt.Fprintln(conn, nil)
+			break
+		}
+
+		resp := Response{Method: PrimeMethodName}
+		// the unmarshaling of req.Number itself guarantees its a number
+		resp.Prime = isPrime(int64(*req.Number))
+		// marshaling of resp will never fail, so ignore the error
+		respBytes, _ := json.Marshal(resp)
+
+		fmt.Fprintln(conn, string(respBytes))
 	}
 }
 
